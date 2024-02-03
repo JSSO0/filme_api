74f1b36bd0f0f65d85476f81823de7b8ecbbfd83
JSSO0 <johnssousa14@gmail.com>
2024-02-02 23:12:06 -0300

    Extração de metodos

--- src/main/groovy/com/api/filme/map/FilmeMap.groovy
+++ src/main/groovy/com/api/filme/map/FilmeMap.groovy
@@ -1,19 +1,18 @@
 package com.api.filme.map
 
 import com.api.filme.model.FilmeModel
+import org.springframework.stereotype.Component
 
+@Component
 class FilmeMap {
 
-        static Map<String, Object> mapearParaParametros(FilmeModel filmeModel) {
-            return [
-                    'id': filmeModel.id,
-                    'nome': filmeModel.nome,
-                    'descricao': filmeModel.descricao,
-                    'duracao': filmeModel.duracao,
-                    'avaliado': filmeModel.avaliado,
-                    'avaliacao': filmeModel.avaliacao
-
-            ] as Map<String, Object>
-        }
+    static Map<String, Object> mapearParaParametros(FilmeModel filmeModel) {
+        ['id': filmeModel.id,
+         'nome': filmeModel.nome,
+         'descricao': filmeModel.descricao,
+         'duracao': filmeModel.duracao,
+         'avaliado': filmeModel.avaliado,
+         'avaliacao': filmeModel.avaliacao]
+    }
 
 }
